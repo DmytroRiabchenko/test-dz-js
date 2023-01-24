69d5f04121cd395f025874497bc4a85113b80ad5
DmytroRiabchenko <dmpmpk@gmail.com>
2023-01-24 20:07:42 +0100

    dz

---
+++ lesson-01/js/index.js
@@ -0,0 +1,19 @@
+// 1.2
+
+// let productName = 'Droid';
+// productName = 'Repair droid';
+// console.log(productName);
+// let pricePerItem = 2000;
+// pricePerItem = 3500;
+// console.log(pricePerItem);
+
+// 1.3
+// Change code below this line
+
+// let topSpeed = 160;
+// let distance = 617.54;
+// const login = 'mango935';
+// const isOnline = true;
+// const isAdmin = false;
+
+// 1.4

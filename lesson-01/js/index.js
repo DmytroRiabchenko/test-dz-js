16e26da82b2185bfa55cc1e211806d5f54baec53
DmytroRiabchenko <dmpmpk@gmail.com>
2023-01-24 21:22:36 +0100

    автопроверка

--- lesson-01/js/index.js
+++ lesson-01/js/index.js
@@ -17,3 +17,26 @@
 // const isAdmin = false;
 
 // 1.4
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// Change code below this line
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// 1.5
+
+// const productName = 'Droid';
+// const pricePerItem = 3500;
+
+// Change code below this line
+// const message = 'You picked Droid, price per item is 3500 credits';
+
+// 1.6
+
+// Change code below this line
+let pricePerDroid = 800;
+let orderedQuantity = 6;
+let deliveryFee = 50;
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+const message =
+  'You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price';

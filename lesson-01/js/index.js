285a48e9ded1e491cb336e04e6860a00cf95e6f3
DmytroRiabchenko <dmpmpk@gmail.com>
2023-01-26 19:58:57 +0100

    16

--- lesson-01/js/index.js
+++ lesson-01/js/index.js
@@ -1,4 +1,4 @@
-// 1.2
+// 1.2;
 
 // let productName = 'Droid';
 // productName = 'Repair droid';
@@ -34,9 +34,134 @@
 // 1.6
 
 // Change code below this line
-let pricePerDroid = 800;
-let orderedQuantity = 6;
-let deliveryFee = 50;
-const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
-const message =
-  'You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price';
+// let pricePerDroid = 800;
+// let orderedQuantity = 6;
+// let deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// 1.7
+// Change code below this line
+// function sayHi() {
+//   console.log('Hello, this is my first function!');
+// }
+// sayHi();
+
+// 1.8
+// Change code below this line
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// Change code above this line
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// 1.9
+
+// function add(a, b, c) {
+// Change code below this line
+
+//   return a + b + c;
+
+// Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// 1.10
+
+// function makeMessage(name, price) {
+// Change code below this line
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+// Change code above this line
+//   return message;
+// }
+// makeMessage(`Radar`, 6150);
+// makeMessage(`Scanner`, 3500);
+// makeMessage(`Reactor`, 8000);
+// makeMessage(`Engine`, 4070);
+
+// 1.11
+
+// function calculateTotalPrice(orderedQuantity, pricePerItem) {
+// Change code below this line
+//   const totalPrice = orderedQuantity * pricePerItem;
+
+// Change code above this line
+//   return totalPrice;
+// }
+// calculateTotalPrice(5, 100);
+
+// 1.12
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+// Change code below this line
+
+//   const message = `You ordered droids worth ${
+//     orderedQuantity * pricePerDroid + deliveryFee
+//   } credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+// Change code above this line
+//   return message;
+// }
+// console.log(makeOrderMessage(2, 100, 50));
+// console.log(makeOrderMessage(4, 300, 100));
+// console.log(makeOrderMessage(10, 70, 200));
+
+// 1.13;
+
+// function isAdult(age) {
+// Change code below this line
+//   const passed = age >= 18;
+
+// Change code above this line
+//   return passed;
+// }
+// console.log(isAdult(20));
+// console.log(isAdult(14));
+// console.log(isAdult(8));
+// console.log(isAdult(37));
+
+// 1.14
+
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+// Change code below this line
+//   const isMatch = password === SAVED_PASSWORD;
+
+// Change code above this line
+//   return isMatch;
+// }
+
+// console.log(isValidPassword(`mangodab3st`));
+// console.log(isValidPassword(`kiwirul3z`));
+// console.log(isValidPassword('jqueryismyjam'));
+
+// 1.15
+
+// function checkAge(age) {
+//   let message;
+
+//   if (age >= 18) {
+//     // Change this line
+
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+
+//   return message;
+// }
+// checkAge(20);
+// checkAge(8);
+// checkAge(14);
+// checkAge(38);
+
+// 1.16

802ee7a7eb8720a3479dbad9cd1dd412b5518584
DmytroRiabchenko <dmpmpk@gmail.com>
2023-02-17 19:38:29 +0100

    Update index.js

--- lesson-01/js/index.js
+++ lesson-01/js/index.js
@@ -710,3 +710,599 @@
 // }
 
 // foo();
+/////////////////////////////////////////////////АВТОПРОВЕРКА/////////////////////////////////////
+//1.1
+
+// function checkAge(age) {
+//   if (age >= 18) {
+//     // Change this line
+//     return 'You are an adult';
+//   }
+//   return 'You are a minor';
+// }
+
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
+
+//1.2
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Welcome!';
+//   }
+//   return 'Access denied, wrong password!';
+
+// }
+
+// 1.3
+
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+
+//   return 'The order is accepted, our manager will contact you';
+// }
+
+// 1.4
+
+//   ОГЛАСИЛ МАСИВ
+
+// 1.5
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// const firstElement = 'apple';
+// const secondElement = 'plum';
+// const lastElement = 'orange';
+
+// 1.6
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+// fruits[1] = 'peach';
+// fruits[3] = 'banana';
+
+// 1.7
+
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+// const fruitsArrayLength = fruits.length;
+
+// 1.8
+
+// const fruits = ['apple', 'peach', 'pear', 'banana'];
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// 1.9
+
+// function getExtremeElements(array) {
+//   // Change code below this line
+//   const deletedSc = [];
+//   deletedSc.push(array[0]);
+//   deletedSc.push(array[array.length - 1]);
+
+//   return deletedSc;
+// }
+
+// getExtremeElements([1, 2, 3, 4, 5]);
+// getExtremeElements(['Earth', 'Mars', 'Venus']);
+// getExtremeElements(['apple', 'peach', 'pear', 'banana']);
+
+// 1.10
+
+// function splitMessage(message, delimiter) {
+//   let words;
+//   // Change code below this line
+//   return message.split(delimiter);
+//   // Change code above this line
+//   return words;
+// }
+
+// 1.11
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//   // Change code below this line
+
+//   return message.split(' ').length * pricePerWord;
+
+//   // Change code above this line
+// }
+
+// console.log(calculateEngravingPrice('JavaScript is in my blood', 10));
+// console.log(calculateEngravingPrice('JavaScript is in my blood', 20));
+// console.log(calculateEngravingPrice('Web-development is creative work', 40));
+// console.log(calculateEngravingPrice('Web-development is creative work', 20));
+
+// 1.12
+
+// function makeStringFromArray(array, delimiter) {
+//   let string;
+//   // Change code below this line
+//   return (messege = array.join(delimiter));
+
+//   // Change code above this line
+//   return string;
+// }
+
+// console.log(
+//   makeStringFromArray(['Mango', 'hurries', 'to', 'the', 'train'], ' ')
+// );
+// console.log(makeStringFromArray(['M', 'a', 'n', 'g', 'o'], ''));
+// console.log(makeStringFromArray(['top', 'picks', 'for', 'you'], '_'));
+
+// 1.13
+
+// function slugify(title) {
+//   // Change code below this line
+
+//   return title.toLowerCase().split(' ').join('-');
+
+//   // Change code above this line
+// }
+
+// console.log(slugify('Arrays for begginers'));
+// console.log(slugify('English for developer'));
+// console.log(slugify('Ten secrets of JavaScript'));
+// console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
+
+// 1.14
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(-3);
+
+// 1.15
+
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Peach', 'Houston'];
+
+// const allClients = oldClients.concat(newClients);
+
+// 1.16
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//   // Change code below this line
+//   let arrays = firstArray.concat(secondArray);
+
+//   if (arrays.length > maxLength) {
+//     return arrays.slice(0, maxLength);
+//   } else {
+//     return arrays;
+//   }
+// }
+// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
+// console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
+// console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
+// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));
+
+// 1.17
+
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) {
+//   // Change this line
+//   console.log(i);
+// }
+
+// 1.18
+
+// function calculateTotal(number) {
+//   // Change code below this line
+//   let total = 0;
+//   for (let i = 1; i <= number; i++) {
+//     total += i;
+//   }
+
+//   return total;
+//   // Change code above this line
+// }
+
+// console.log(calculateTotal(1));
+// console.log(calculateTotal(3));
+// console.log(calculateTotal(7));
+// console.log(calculateTotal(18));
+// console.log(calculateTotal(24));
+
+// 1.19
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// for (let i = 0; i < fruits.length; i += 1) {
+//   // Change this line
+//   const fruit = fruits[i]; // Change this line
+//   console.log(fruit);
+// }
+
+// 1.20
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+//   for (let orders of order) {
+//     total += orders;
+//   }
+//   // Change code above this line
+//   return total;
+// }
+
+// 1.21
+
+// function findLongestWord(string) {
+//   let words = string.split(' ');
+//   let longestWord = '';
+//   for (let i = 0; i < words.length; i++) {
+//     if (words[i].length > longestWord.length) {
+//       longestWord = words[i];
+//     }
+//   }
+//   return longestWord;
+// }
+
+// 1.22
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   for (let i = min; i <= max; i += 1) {
+//     numbers.push(i);
+//   }
+//   // Change code above this line
+//   return numbers;
+// }
+// console.log(createArrayOfNumbers(1, 3));
+// console.log(createArrayOfNumbers(14, 17));
+// console.log(createArrayOfNumbers(29, 34));
+
+//второй вариант решения............................................
+
+// function createArrayOfNumbers(min, max) {
+//   let numbers = [];
+//   for (let i = min; i <= max; i++) {
+//     numbers.push(i);
+//   }
+//   return numbers;
+// }
+
+// 1.23
+
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   let newNumber = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > value) {
+//       newNumber.push(numbers[i]);
+//     }
+//   }
+//   return newNumber;
+// }
+
+// // Change code above this line
+// 1.24
+// function checkFruit(fruit) {
+//   const fruits = ['apple', 'plum', 'pear', 'orange'];
+//   return; // Change this line
+// }
+// console.log(fruits.includes('plum'));
+
+// 1.25
+
+// 1.26
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+
+//   for (let orders of order) {
+//     total += orders;
+//   }
+
+//   return total;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+
+// 1.27
+
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   const number = numbers[i];
+
+//   for (let number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+//   // Change code above this line
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+
+// 1.28
+
+// const a = 3 % 1;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 6;
+
+// 1.29
+
+// function getEvenNumbers(start, end) {
+//   // Change code below this line
+
+//   const numbers = [];
+
+//   for (let i = start; i <= end; i += 1) {
+//     if (i % 2 === 0) {
+//       numbers.push(i);
+//     }
+//   }
+//   return numbers;
+//   // Change code above this line
+// }
+
+// console.log(getEvenNumbers(2, 5));
+
+// 1.30
+
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
+
+// 1.31
+
+// function findNumber(start, end, divisor) {
+//   // Change code below this line
+//   let number;
+
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       number = i;
+//       return number;
+//     }
+//   }
+
+// }
+
+// 1.32
+
+// function includes(array, value) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+//============================================ 3 МОДУЛЬ============================================//
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+
+// for (const key of keys) {
+//   // Ключ
+//   console.log(key);
+//   // Значення властивості
+//   console.log(book[key]);
+// }
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'rating']
+
+// const values = Object.values(book);
+// console.log(values); // ['The Last Kingdom', 'Bernard Cornwell', 8.38]
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'На березі спокійних вод',
+//     author: 'Роберт Шеклі',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'Сон смішної людини',
+//     author: 'Федір Достоєвський',
+//     rating: 7.75,
+//   },
+// ];
+// for (const book of books) {
+//   // Об'єкт книги
+//   console.log(book);
+//   // Назва
+//   console.log(book.title);
+//   // Автор
+//   console.log(book.author);
+//   // Рейтинг
+//   console.log(book.rating);
+// }
+// function multiply(firstNumber, secondNumber, ...otherArgs) {
+//   console.log(firstNumber); // Значення першого аргументу
+//   console.log(secondNumber); // Значення другого аргументу
+//   console.log(otherArgs); // Масив інших аргументів
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+//===================================================================================
+// function doStuffWithBook(book) {
+//   const { title, numberOfPages, downloads, rating, isPublic } = book;
+//   console.log(title);
+//   console.log(numberOfPages);
+//   // І так далі
+// }
+// doStuffWithBook({
+//   title: 'The Last Kingdom',
+//   numberOfPages: 736,
+//   downloads: 10283,
+//   rating: 8.38,
+//   isPublic: true,
+// });
+//====================================================================================
+
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
+
+// const credentials = {
+//   [emailInputName]: 'henry.carter@aptmail.com',
+//   [passwordInputName]: 'jqueryismyjam',
+
+//   // Change code below this line
+// };
+// console.log(credentials.email);
+// console.log(credentials.password);
+
+// // Change code above this line
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+// console.log(keys);
+// console.log(values);
+//===================================================================
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// const {
+//   today: { low: lowToday, high: highToday, icon: todayIcon },
+//   tomorrow: {
+//     high: highTomorrow,
+//     low: lowTomorrow,
+//     icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+//   },
+// } = forecast;
+//=====================================ПРИМЕР ПОДСЧЁТА СУММЫ=======================================
+// Change code below this line
+// function add(...args) {
+//   let result = 0;
+//   for (arg of args) {
+//     result += arg;
+//   }
+//   return result;
+// }
+// add(15, 27);
+// add(12, 4, 11, 48);
+// add(32, 6, 13, 19, 8);
+// add(74, 11, 62, 46, 12, 3);
+
+//======================================
+// const bookShelf = {
+//   // Change code below this line
+//   books: ["The last kingdom", "The guardian of dreams"],
+//   getBooks() {
+//     return "Returning all books";
+//   },
+//   addBook(bookName) {
+//     return `Adding book ${bookName}`;
+//   },
+//   // Change code above this line
+//   removeBook(bookName) {
+//     return `Deleting book ${bookName}`;
+//   },
+//   updeteBook(oldName, newName) {
+//   return `Updating book ${oldName} to ${newName}`;
+//   }
+// }
+//==========================================
+// const atTheOldToad = {
+//   potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+//   updatePotionName(oldName, newName) {
+//     // Change code below this line
+//     let index = this.potions.indexOf(oldName);
+//     if (index !== -1) {
+//       this.potions[index] = newName;
+//     }
+//   },
+// };
+//================================================ 41 ЗАДАЧА, 3 модуль=========================
+// const atTheOldToad = {
+//   potions: [
+//     { name: 'Speed potion', price: 460 },
+//     { name: 'Dragon breath', price: 780 },
+//     { name: 'Stone skin', price: 520 },
+//   ],
+//   // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     for (const obj of this.potions) {
+//       if (obj.name === newPotion.name) {
+//         return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//       }
+//     }
+
+//     this.potions.push(newPotion);
+//   },
+//   removePotion(potionName) {
+//     for (const names of this.potions) {
+//       const potionIndex = this.potions.indexOf(names);
+//       if (names.name === potionName) {
+//         this.potions.splice(potionIndex, 1);
+//       }
+//     }
+//     return this.potions;
+//   },
+//   updatePotionName(oldName, newName) {
+//     for (const elements of this.potions) {
+//       if (elements.name === oldName) {
+//         elements.name = newName;
+//       }
+//     }
+//   },
+// };
